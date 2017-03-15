7f9478a1ba75c81b7da4caaf04575c5a76357091
mifisher <mifisher@pivotal.io>
2017-03-15 16:04:08 -0700

    Chapter 5 - Individual Object Pages

--- collection/views.py
+++ collection/views.py
@@ -5,3 +5,9 @@ from collection.models import Thing
 def index(request):
     things = Thing.objects.all()
     return render(request, 'index.html', {'things': things,})
+
+def thing_detail (request, slug):
+    thing = Thing.objects.get(slug=slug)
+
+    return render(request, 'things/thing_detail.html', {'thing': thing,})
+
