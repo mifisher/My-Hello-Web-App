5fd838c9a207f140f854c5bfdf8d451c0888cfd1
mifisher <mifisher@pivotal.io>
2017-03-15 11:28:19 -0700

    Chapter 3 - Adding Dynamic Data

--- collection/models.py
+++ collection/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Thing (models.Model):
+    name = models.CharField (max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
