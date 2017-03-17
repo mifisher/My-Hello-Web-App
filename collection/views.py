7ad064272954c90a9bae444642dedc19d6c61c3d
mifisher <mifisher@pivotal.io>
2017-03-16 17:05:38 -0700

    Chapter 7: Adding a Registration Page

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from collection.models import Thing
 from collection.forms import ThingForm
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -11,8 +14,13 @@ def thing_detail (request, slug):
     thing = Thing.objects.get(slug=slug)
     return render(request, 'things/thing_detail.html', {'thing': thing, })
 
+@login_required
 def edit_thing(request, slug):
     thing = Thing.objects.get(slug=slug)
+
+    if thing.user != request.user:
+        raise Http404
+
     form_class = ThingForm
 
     if request.method == 'POST':
@@ -26,3 +34,21 @@ def edit_thing(request, slug):
         form = form_class(instance=thing)
         return render(request, 'things/edit_thing.html', {'thing': thing, 'form': form, })
 
+def create_thing(request):
+    form_class = ThingForm
+
+    if request.method == 'POST':
+        form = form_class(request.POST)
+
+        if form.is_valid():
+            thing = form.save(commit=False)
+
+            thing.user = request.user
+            thing.slug = slugify(thing.name)
+            thing.save()
+
+            return redirect('thing_detail', slug=thing.slug)
+    else:
+        form = form_class()
+
+    return render(request, 'things/create_thing.html', {'form': form, })
